0545b6ed4826e0582922c1934a954e864c39a8eb
weibenfalk <thomas.weibenfalk@gmail.com>
2022-07-15 12:56:46 +0200

    removed static server adress

--- api/fetchFunctions.ts
+++ api/fetchFunctions.ts
@@ -1,9 +1,6 @@
 // Types
 import type { Movies } from './types';
 
-const dev = process.env.NODE_ENV !== 'production';
-export const server = dev ? 'http://localhost:3000' : 'https://react-movie-db-v4.vercel.app/';
-
 // As we're in a .ts file we can just use <returnType>. But if this was a .tsx file we have to
 // use <returnType, >
 export const basicFetch = async <returnType>(endpoint: string): Promise<returnType> => {
@@ -18,5 +15,5 @@ export const basicFetch = async <returnType>(endpoint: string): Promise<returnTy
 
 // Fetch functions
 export const fetchMovies = async (search = '', page = 1): Promise<Movies> => {
-  return await (await fetch(`${server}/api/movies?search=${search}&page=${page}`)).json();
+  return await (await fetch(`/api/movies?search=${search}&page=${page}`)).json();
 };
